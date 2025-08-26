919f94f1b42c7de7c52cd24a40eb04d710661ff1
Si-xiyu <junxiangluo0@gmail.com>
2025-08-26 19:42:02 +0800

    exercise 1 finish

--- lab2setup/Dog/DogTest.java
+++ lab2setup/Dog/DogTest.java
@@ -3,6 +3,8 @@ package Dog;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
+// 在测试中，需要有三部分：测试用例、调用方法、断言
+// 其中，断言是用于检查条件是否为真的语句
 public class DogTest {    
     @Test
     public void testSmall() {
