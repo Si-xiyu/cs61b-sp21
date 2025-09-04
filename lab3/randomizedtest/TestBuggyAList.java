15cacff5fb54536fba8208f5ca46b955f442dfaa
Si-xiyu <junxiangluo0@gmail.com>
2025-09-04 16:16:31 +0800

    finish all the tasks

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -1,6 +1,7 @@
 package randomizedtest;
 
 import edu.princeton.cs.algs4.StdRandom;
+import org.junit.Assert;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -36,28 +37,45 @@ public class TestBuggyAList {
     @Test
     public void randomizedTest(){
         AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> B = new BuggyAList<>();
 
-        int N = 500;
+        int N = 5000;
         for (int i = 0; i < N; i += 1) {
             int operationNumber = StdRandom.uniform(0, 4);
             if (operationNumber == 0) {
                 // addLast
                 int randVal = StdRandom.uniform(0, 100);
                 L.addLast(randVal);
-                System.out.println("addLast(" + randVal + ")");
+                B.addLast(randVal);
             } else if (operationNumber == 1) {
                 // size
-                int size = L.size();
-                System.out.println("size: " + size);
+                int size1 = L.size();
+                int size2 = B.size();
+                assertEquals(size1,size2);
             } else if (operationNumber == 2) {
                 // getLast
-                if(L.size() == 0){continue;}
-                int output = L.getLast();
-                System.out.println(output);
+                if(L.size() == 0 || B.size() == 0){
+                    continue;
+                }
+
+                int output1 = L.getLast();
+                int output2 = B.getLast();
+                assertEquals(output1,output2);
+
             } else if (operationNumber == 3) {
                 // removeLast
-                if(L.size() == 0){continue;}
-                L.removeLast();
+
+                if(L.size() == 0){
+                    continue;
+                }
+
+                if(B.size() ==0){
+                    continue;
+                }
+
+                int Last1 = L.removeLast();
+                int Last2 = B.removeLast();
+                assertEquals(Last1,Last2);
             }
         }
     }
